eb3c1e9620a51676f0ff4ddf69fa4d5a337ade97
eldraelin <eldraelin@3c9f6983-0faa-fce9-b004-d3af0a5b9716>
2010-11-04 06:08:37 +0000

    capture de la taille de la matrice dans le parser

--- DistanceVector/src/distancevector/Parser.java
+++ DistanceVector/src/distancevector/Parser.java
@@ -19,6 +19,11 @@ public class Parser {
    Constructor.
    @param aFileName full name of an existing, readable file.
   */
+
+  private int taille_Matrice;
+
+  private final File fFile;
+
   public Parser(String aFileName){
     fFile = new File(aFileName);
   }
@@ -41,6 +46,30 @@ public class Parser {
     }
   }
 
+public final void initialRound() throws FileNotFoundException{
+     Scanner scanner = new Scanner(new FileReader(fFile));
+     int k=0;
+     try {
+      //first use a Scanner to get each line
+      while ( scanner.hasNextInt() ){
+      k++;
+       System.out.println("Number found:"+scanner.nextInt()+ "Number of ints"+ k);
+      }}
+
+     finally {
+      //ensure the underlying stream is always closed
+      //this only has any effect if the item passed to the Scanner
+      //constructor implements Closeable (which it does in this case).
+      scanner.close();
+    }
+   if (Math.floor(java.lang.Math.sqrt(k))==java.lang.Math.sqrt(k))
+   {this.taille_Matrice= (int) java.lang.Math.sqrt(k);}
+   else
+   {
+   //Some message in the ihm.
+         ;
+   }
+}
   /**
    Overridable method for processing lines in different ways.
 
@@ -54,20 +83,24 @@ public class Parser {
   protected void processLine(String aLine){
     //use a second Scanner to parse the content of each line
     Scanner scanner = new Scanner(aLine);
-    scanner.useDelimiter("=");
+    int k=0;
     if ( scanner.hasNext() ){
-      String name = scanner.next();
-      String value = scanner.next();
-      log("Name is : " + quote(name.trim()) + ", and Value is : " + quote(value.trim()) );
+    k++;
+
+      
+    
+      
+      
+      /*  log("Name is : " + quote(name.trim()) + ", and Value is : " + quote(value.trim()) );
     }
     else {
       log("Empty or invalid line. Unable to process.");
     }
     //no need to call scanner.close(), since the source is a String
-  }
+ */
+    }}
 
-  // PRIVATE
-  private final File fFile;
+ 
 
   private static void log(Object aObject){
     System.out.println(String.valueOf(aObject));
