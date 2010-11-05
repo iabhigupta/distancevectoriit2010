23c291713691d21ba089f712d22a492b36e44289
eldraelin <eldraelin@3c9f6983-0faa-fce9-b004-d3af0a5b9716>
2010-11-05 22:10:06 +0000

    Avec un main plus propre

--- trunk/DistanceVector/src/distancevector/Main.java
+++ trunk/DistanceVector/src/distancevector/Main.java
@@ -23,28 +23,14 @@ public class Main {
      */
     public static void main(String[] args) throws FileNotFoundException {
       new Menu();
-   
+
+
+      /* Code pour récupérer la matrice générée par le parser
     Parser test =new Parser("C:/Users/Klem/Documents/test.txt");
-  /*
-    test.initialRound();
-  
-    int[][] matrice = new int[test.getTaille_Matrice()][test.getTaille_Matrice()];
-    test.processLineByLine();
-    matrice = test.getMatrice();
-  
-
-   
-   */
-   /* test.instantiateMatrice();
+    test.instantiateMatrice();
     int[][] matrice = test.getMatrice();
-     for (int i=0;i<test.getTaille_Matrice();i++){
-        for (int j=0;j<test.getTaille_Matrice();j++){
-            System.out.print(matrice[i][j]+" ");
-        }
-        System.out.println("\n");
-    }
-
     */
+    
    }
     
 
