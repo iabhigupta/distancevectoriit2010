a6e4d8de9b476351fd2274b9aa0c3ab8ffcffc7c
eldraelin <eldraelin@3c9f6983-0faa-fce9-b004-d3af0a5b9716>
2010-11-05 22:31:14 +0000

    Message en cas de fichier invalide

---
+++ trunk/DistanceVector/src/distancevector/InvalidFileException.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package distancevector;
+
+/**
+ *
+ * @author Klem
+ */
+public class InvalidFileException extends Exception {
+
+    public InvalidFileException() {
+    }
+
+    
+
+
+}
